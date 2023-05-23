3bbf8f9bc72aa2230cbc2cb0392b6cb949da7705
rmadbstjd <rmadbstjd@naver.com>
2023-05-23 16:20:33 +0900

    fix : import 경로 변경

--- src/components/Admin/Inputs/InputLayout/index.jsx
+++ src/components/Admin/Inputs/InputLayout/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as Style from "./styles";
 import FileInput from "../FileInput";
-import TextInput from "../../TextInput";
+import TextInput from "../TextInput";
 const InputProduct = ({
   product,
   file,
